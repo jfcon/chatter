6abb0f5f1a65cd719df486628495204cc426824d
John <connollyjohnf@gmail.com>
2019-03-12 19:13:56 +0000

    passed down hardcoded usernames and messages down to their child components

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,19 @@ import Message from "./Message.jsx";
 import Nav from "./Nav.jsx";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      messages: [{ id: 1, user: "Bobby", content: "hello" }, { id: 2, user: "Sally", content: "a/s/l?" }],
+      currentUser: ""
+    };
+  }
   render() {
     return (
       <div>
         <Nav />
-        <Message />
-        <ChatBar />
+        <Message messages={this.state.messages} />
+        <ChatBar currentUser={this.state.currentUser} />
       </div>
     );
   }
