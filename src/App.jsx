f7b5e90bf9314795ba092fec04192eed779b83df
John <connollyjohnf@gmail.com>
2019-03-12 01:56:04 +0000

    separated message, messageList, and nav into their own components, and imported them to the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,14 @@
 import React, { Component } from "react";
 import ChatBar from "./ChatBar.jsx";
+import Message from "./Message.jsx";
+import Nav from "./Nav.jsx";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar">
-          <a href="/" className="navbar-brand">
-            Chatty
-          </a>
-        </nav>
-        <main className="messages">
-          <div className="message">
-            <span className="message-username">Anonymous1</span>
-            <span className="message-content">I won't be impressed with technology until I can download food.</span>
-          </div>
-          <div className="message system">Anonymous1 changed their name to nomnom.</div>
-        </main>
+        <Nav />
+        <Message />
         <ChatBar />
       </div>
     );
